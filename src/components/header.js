2150d98fc2589ac9d37b3e4ac5e5d18bc3fbf20d
wormyn <87888767+wormyn@users.noreply.github.com>
2022-02-10 20:07:24 -0800

    task one finished

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,25 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  const header = document.createElement('div')
+  const dateInfo = document.createElement('span')
+  const titleInfo = document.createElement('h1')
+  const tempInfo = document.createElement('span')
+
+  dateInfo.textContent = date
+  titleInfo.textContent = title
+  tempInfo.textContent = temp
+
+  header.appendChild(dateInfo)
+  header.appendChild(titleInfo)
+  header.appendChild(tempInfo)
+
+  header.classList.add('header')
+  dateInfo.classList.add('date')
+  tempInfo.classList.add('temp')
+
+  return header
 }
 
 const headerAppender = (selector) => {
